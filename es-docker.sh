898656718e8b9fe786ff96fac9cc61ce55b895b8
Tony133 <a.tripodi133@gmail.com>
2026-08-17 23:33:16 +0200

    fix: update es-docker.sh

--- es-docker.sh
+++ es-docker.sh
@@ -3,9 +3,15 @@
 # run this file to get a working ES instance
 # for running the test locally
 
+# ES_VERSION can be overridden:
+# ES_VERSION=8.19.19 ./es-docker.sh
+# ES_VERSION=9.4.4 ./es-docker.sh
+
+ES_VERSION="${ES_VERSION:-8.19.19}"
+
 exec docker run \
   --rm \
   -e "discovery.type=single-node" \
   -e "xpack.security.enabled=false" \
   -p 9200:9200 \
-  docker.elastic.co/elasticsearch/elasticsearch:8.15.2
+  "docker.elastic.co/elasticsearch/elasticsearch:${ES_VERSION}"
